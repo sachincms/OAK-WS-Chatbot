a718135719fa23656171abd7fe2cc12789b2583f
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-04-23 15:14:50 +0530

    updated chat_app with login

--- chat_app.py
+++ chat_app.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from utils.chat import qa_chat_with_prompt, stream_data
 from utils.image_processing import display_images
 from utils.files_processing import convert_excel_to_dict
+from utils.auth_db import init_db, authenticate_user, add_user, get_user_role, get_all_users, approve_user, promote_user_to_admin, delete_user
 import warnings
 warnings.simplefilter("ignore", ResourceWarning)
 import asyncio
@@ -13,15 +14,12 @@ import logging
 import uuid
 from streamlit.web.server.websocket_headers import _get_websocket_headers
 from traceloop.sdk import Traceloop
+from time import sleep
 import json
+from logging_config import get_logger
+import os
 
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - File: %(filename)s - %(levelname)s - %(message)s', 
-    datefmt='%d/%m/%Y %I:%M:%S %p',
-)
-
+logger = get_logger(__name__)
 
 st.set_page_config(
     page_title="SPF chatbot",
@@ -30,122 +28,160 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+
 display_images(SWASTI_LOGO_PATH, SPF_LOGO_PATH)
+
+
+init_db()
+
+if "authenticated" not in st.session_state:
+    st.session_state["authenticated"] = False
+
+if not st.session_state["authenticated"]:
+    login_tab, register_tab = st.tabs(["Login", "Register"])
+
+    with login_tab:
+        st.subheader("Login")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type = "password")
+        if st.button("Login"):
+            if authenticate_user(username, password):
+                st.success("Login successful")
+                st.session_state["authenticated"] = True
+                st.session_state["username"] = username
+            else:
+                st.error("Invalid credentials")
+
+    with register_tab:
+        st.subheader("Register")
+        new_username = st.text_input("New Username")
+        new_password = st.text_input("New Password", type = "password")
+        if st.button("Register"):
+            if add_user(new_username, new_password):
+                st.success("User registered....")
+            else:
+                st.error("Username already exists or error occured")
+
+
+
+
 # st.write(INTRO_MESSAGE)
 # st.markdown(INTRO_MARKDOWN, unsafe_allow_html=True)
 
-context_options = {
-    "All Phases": ALL_PHASES_JSON_FILE_PATH,
-    "Phase 1": PHASE1_JSON_FILE_PATH,
-    "Phase 2": PHASE2_WITH_SDD_JSON_FILE_PATH,
-}
-
-_, col2, _ = st.columns(3)
-
-with col2:
-    context = st.selectbox(
-        "Select Phase: ",
-        context_options,
-    )
-
-if "previous_context" not in st.session_state:
-    st.session_state["previous_context"] = context
-
-if st.session_state["previous_context"] != context:
-
-    st.session_state["chat_history"] = [
-        {"role": "user", "message": DEFAULT_QUERY},
-        {"role": "assistant", "message": DEFAULT_FINAL_RESPONSE},
-    ]
-
-    st.info(f"Changed context from {st.session_state["previous_context"]} to {context}.")
-
-    st.session_state["previous_context"] = context
-    
-    file = context_options[context]
-    text = json.load(open(file, "r", encoding="utf-8"))
-    st.session_state["oak_data"] = text
-
-try:
-    loop = asyncio.get_running_loop()
-    loop.close()
-except RuntimeError:
-    pass
+if st.session_state["authenticated"]:
+    st.write(f"Welcome {st.session_state["username"]}")
 
 
-# if "oak_data" not in st.session_state:
-    # st.session_state["oak_data"] = None
-file = context_options[context]
-text = json.load(open(file, "r", encoding="utf-8"))
-st.session_state["oak_data"] = text
+    context_options = {
+        "All Phases": ALL_PHASES_JSON_FILE_PATH,
+        "Phase 1": PHASE1_JSON_FILE_PATH,
+        "Phase 2": PHASE2_WITH_SDD_JSON_FILE_PATH,
+    }
 
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
+    _, col2, _ = st.columns(3)
 
-# if st.session_state["oak_data"] is None:
-#     with st.spinner("Loading excel data...."):
-#         # text = convert_excel_to_dict(spreadsheet_id = SPREADSHEET_ID,
-#         #                                         json_file_path = JSON_FILE_PATH,
-#         #                                         gdrive_credentials_path = GOOGLE_DRIVE_CREDENTIALS_PATH)
-#         st.session_state["oak_data"] = text
+    with col2:
+        context = st.selectbox(
+            "Select Phase: ",
+            context_options,
+        )
 
-if "chat_container" not in st.session_state:
-    st.session_state.chat_container = st.container()
+    if "previous_context" not in st.session_state:
+        st.session_state["previous_context"] = context
 
-if "messages" not in st.session_state.keys():
-    if "session_id" not in st.session_state.keys():
-        session_id = "uuid-" + str(uuid.uuid4())
-
-        logging.info(json.dumps({"_type": "set_session_id", "session_id": session_id}))
-        Traceloop.set_association_properties({"session_id": session_id})
-        st.session_state["session_id"] = session_id
+    if st.session_state["previous_context"] != context:
 
         st.session_state["chat_history"] = [
             {"role": "user", "message": DEFAULT_QUERY},
             {"role": "assistant", "message": DEFAULT_FINAL_RESPONSE},
         ]
 
-    #TODO: _get_websocket_headers is deprecated. Replace it with st.context.headers but even this is not JSON serializable so find a solution.
-    # headers = _get_websocket_headers()
-    # logging.info(
-    #     json.dumps(
-    #         {
-    #             "_type": "set_headers",
-    #             "headers": headers,
-    #             "session_id": st.session_state.get("session_id", "NULL_SESS"),
-    #         }
-    #     )
-    # )
+        st.info(f"Changed context from {st.session_state["previous_context"]} to {context}.")
 
-if query:= st.chat_input("Ask a question"):
-    st.session_state["chat_history"].append({
-        "role": "user", 
-        "message": query, 
-    })
-
-for chat in st.session_state["chat_history"]:
-    with st.chat_message(chat["role"]):
-        st.write(chat["message"])
-
-if st.session_state["chat_history"][-1]["role"] != "assistant":  
-    try:
-
-        response = qa_chat_with_prompt(text = text, query = query)
-        answer = response["answer"]
-        source = response["source"]
-        full_response = f"\n{answer}\n\n**Source:** {source}"
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                st.write_stream(stream_data(full_response))
-                st.session_state["chat_history"].append({
-                    "role": "assistant", 
-                    "message": full_response, 
-                })
+        st.session_state["previous_context"] = context
         
-    except Exception as e:
-        logging.error(e)
+        file = context_options[context]
+        text = json.load(open(file, "r", encoding="utf-8"))
+        st.session_state["oak_data"] = text
+
+    try:
+        loop = asyncio.get_running_loop()
+        loop.close()
+    except RuntimeError:
+        pass
+
+
+
+    file = context_options[context]
+    text = json.load(open(file, "r", encoding="utf-8"))
+    st.session_state["oak_data"] = text
+
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
+
+
+
+    if "chat_container" not in st.session_state:
+        st.session_state.chat_container = st.container()
+
+    if "messages" not in st.session_state.keys():
+        if "session_id" not in st.session_state.keys():
+            session_id = "uuid-" + str(uuid.uuid4())
+
+            logging.info(json.dumps({"_type": "set_session_id", "session_id": session_id}))
+            Traceloop.set_association_properties({"session_id": session_id})
+            st.session_state["session_id"] = session_id
+
+            st.session_state["chat_history"] = [
+                {"role": "user", "message": DEFAULT_QUERY},
+                {"role": "assistant", "message": DEFAULT_FINAL_RESPONSE},
+            ]
+
+        #TODO: _get_websocket_headers is deprecated. Replace it with st.context.headers but even this is not JSON serializable so find a solution.
+        # headers = _get_websocket_headers()
+        # logging.info(
+        #     json.dumps(
+        #         {
+        #             "_type": "set_headers",
+        #             "headers": headers,
+        #             "session_id": st.session_state.get("session_id", "NULL_SESS"),
+        #         }
+        #     )
+        # )
+
+    if query:= st.chat_input("Ask a question"):
         st.session_state["chat_history"].append({
-            "role": "assistant",
-            "message": ERROR_MESSAGE,
+            "role": "user", 
+            "message": query, 
         })
-        st.chat_message("assistant").write_stream(stream_data(ERROR_MESSAGE))
+
+    for chat in st.session_state["chat_history"]:
+        with st.chat_message(chat["role"]):
+            st.write(chat["message"])
+
+    if st.session_state["chat_history"][-1]["role"] != "assistant":  
+        try:
+            response = qa_chat_with_prompt(text = text, query = query)
+            answer = response["answer"]
+            source = response["source"]
+            full_response = f"\n{answer}\n\n**Source:** {source}"
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    st.write_stream(stream_data(full_response))
+                    st.session_state["chat_history"].append({
+                        "role": "assistant", 
+                        "message": full_response, 
+                    })
+            
+        except Exception as e:
+            logging.error(e)
+            st.session_state["chat_history"].append({
+                "role": "assistant",
+                "message": ERROR_MESSAGE,
+            })
+            st.chat_message("assistant").write_stream(stream_data(ERROR_MESSAGE))
+
+
+if st.button("Logout"):
+    st.session_state["authenticated"]= False
+    st.rerun()
