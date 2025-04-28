ff0bd67c08614d31a368b3c3a81a4cc2a8fdf08e
sachincms <sachin.r@catalysts.org>
2025-04-28 14:55:46 +0530

    added validation and db handler usage

--- utils/auth_db.py
+++ utils/auth_db.py
@@ -1,98 +1,112 @@
-import sqlite3
-from pymongo import MongoClient
 from bson.objectid import ObjectId
 import bcrypt
 import pandas as pd
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from config import MONGODB_URI, DATABASE, COLLECTION
+from config import USER_COLLECTION
+from models.user import UserSchema, validate_user
+from handlers.MongoDBHandler import MongoDBHandler
 from logging_config import get_logger
 
 
+mongodb_handler = MongoDBHandler(USER_COLLECTION)
 logger = get_logger(__name__)
 
-client = MongoClient(MONGODB_URI)
-db = client[DATABASE]
-users_collection = db[COLLECTION]
 
 def init_db():
-    users_collection.create_index("username", unique = True)
+    mongodb_handler.create_index("username")
 
+def get_usernames():
+    users = list(mongodb_handler.read_data({}))
+    return [user["username"] for user in users]
 
-def add_user(username: str, password: str):
-    password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-    try:
-        user_count = users_collection.count_documents({})
-
-        # the first user will be automatically assigned as admin
-        if user_count == 0:
-            role = "admin"
-        else:
-            role = "user"
-
-        if role == "admin":
-            status = "approved"
-        else:
-            status = "pending"
-
-        users_collection.insert_one({
-            "username": username,
-            "password_hash": password_hash,
-            "role": role,
-            "status": status
-        })
-        
-        return True
+def add_user(user_data: dict):
+    if user_data["username"] in get_usernames():
+        return False
     
-    except Exception as e:
-        logger.error(f"Unexpected error: {e}")
+    password_hash = bcrypt.hashpw(user_data["password"].encode(), bcrypt.gensalt())
+
+    try:
+        if validate_user(user_data):
+            data_to_insert = {
+                "full_name": user_data["full_name"],
+                "username": user_data["username"],
+                "email": user_data["email"],
+                "password_hash": password_hash,
+                "created_at": user_data["created_at"],
+                "role": "user",
+                "status": "pending"
+            }
+            print(data_to_insert)
+
+            mongodb_handler.insert_data(data_to_insert)
+            
+            return True
+        
+        else:
+            logger.error(f"Cannot Add User {user_data["username"]}:  Validation Failed.")
+            return False
+    
+    except Exception as ex:
+        logger.error(f"Unexpected Error: {ex}")
         return False
    
     
 
 def authenticate_user(username: str, password: str):
-    user = users_collection.find_one({"username": username})
+    documents = mongodb_handler.read_data({"username": username})
+    if len(documents) == 0:
+        logger.error(f"User {username} not found.")
+        return False
+    
+    user = documents[0]
     if user and user.get("status") == "approved":
-        return bcrypt.checkpw(password.encode(), user.get("password_hash").encode())
+        return bcrypt.checkpw(password.encode(), user.get("password_hash"))
     return False
 
 
 def get_all_users():
-    users = list(users_collection.find({}, {"username": 1, "role": 1, "status": 1}))
+    users = list(mongodb_handler.read_data())
     return pd.DataFrame(users)
 
 
 def approve_user(user_id: str):
     try:
-        result = users_collection.update_one(
+        mongodb_handler.update_data(
             {"_id": ObjectId(user_id)},
-            {"$set": {"status": "approved"}}
+            {"status": "approved"}
         )
-        logger.info(f"Approved user ID: {user_id}")
-    except Exception as e:
-        logger.error(f"Error approving user {user_id}: {e}")
+        logger.info(f"Approved User ID: {user_id}")
+        return True
+    except Exception as ex:
+        logger.error(f"Error Approving User {user_id}: {ex}")
+        return False
     
 
 
 def get_user_role(username: str):
-    user = users_collection.find_one({"username": username})
+    documents = mongodb_handler.read_data({"username": username})
+    if len(documents) == 0:
+        logger.error(f"User {username} not found.")
+        return None
+    user = documents[0]
     return user.get("role") if user else None
 
 
 def promote_user_to_admin(user_id: str):
     try:
-        users_collection.update_one(
+        mongodb_handler.update_data(
             {"_id": ObjectId(user_id)},
-            {"$set": {"role": "admin"}}
+            {"role": "admin"}
         )
-    except Exception as e:
-        logger.error(f"Error promoting user {user_id}: {e}")
+    except Exception as ex:
+        logger.error(f"Error Promoting User {user_id}: {ex}")
     
     
 def delete_user(user_id: str):
     try:
-        users_collection.delete_one({"_id": ObjectId(user_id)})
-        logger.info(f"Deleted user id: {user_id}")
-    except Exception as e:
-        logger.error(f"Error deleting user: {e}")
+        mongodb_handler.delete_data({"_id": ObjectId(user_id)})
+        logger.info(f"Deleted User ID: {user_id}")
+    except Exception as ex:
+        logger.error(f"Error Deleting User: {ex}")
