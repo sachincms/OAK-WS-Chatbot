656c6c94d42333b2eb6fcbe5722f8dfdf62bc153
sachincms <sachin.r@catalysts.org>
2025-04-30 16:37:03 +0530

    cosmetic changes

--- navigation.py
+++ navigation.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import os
 
 pages = [
-    st.Page("chat_app.py", title = "Chatbot"),
-    st.Page("case_stories.py", title = "Case Stories"),
-    st.Page("manage_users.py", title = "User Management")
+    st.Page(os.path.join("app_pages", "chatbot.py"), title = "Chatbot"),
+    st.Page(os.path.join("app_pages", "case_stories.py"), title = "Case Stories"),
+    st.Page(os.path.join("app_pages", "user_management.py"), title = "User Management")
 ]
 
 pg = st.navigation(pages)
