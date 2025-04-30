cda13b5e60282ca6d8d669c100baec85e1715519
sahilgk-cms <sahil@catalysts.org>
2025-04-30 13:10:41 +0530

    added case stories page

--- navigation.py
+++ navigation.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 pages = [
     st.Page("chat_app.py", title = "Chatbot"),
-    st.Page("manage_users.py", title = "User Management"),
+    st.Page("case_stories.py", title = "Case Stories"),
+    st.Page("manage_users.py", title = "User Management")
 ]
 
 pg = st.navigation(pages)
