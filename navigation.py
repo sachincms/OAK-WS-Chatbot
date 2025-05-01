254598005e9f15a730dbd47b06cb6481bc03973c
sachincms <sachin.r@catalysts.org>
2025-05-01 11:50:13 +0530

    changed default page to Home

--- navigation.py
+++ navigation.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 pages = [
-    st.Page(os.path.join("app_pages", "chatbot.py"), title = "Chatbot"),
+    st.Page(os.path.join("app_pages", "chatbot.py"), title = "Home"),
     st.Page(os.path.join("app_pages", "case_stories.py"), title = "Case Stories"),
     st.Page(os.path.join("app_pages", "user_management.py"), title = "User Management")
 ]
